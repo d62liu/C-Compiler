55cb6c3dae5cb8ddeabae3c3de2972c9920daf84
d62liu <liudaniel0322@gmail.com>
2025-06-20 18:56:44 -0400

    added token types

--- lexer.py
+++ lexer.py
@@ -1,4 +1,6 @@
 
+import enum
+
 class lexer:
     def __init__(self, source):
         self.source = source + "\n"
@@ -18,12 +20,51 @@ class lexer:
     def return_error(self):
         pass
     def skip_whitespace(self):
-        pass
-    def skip_comments(self):
-        pass
+        while self.source[self.cur_pos] == " " or self.source[self.cur_char] == "\n":
+            self.next_char()
+        self.cur_char = self.source[self.cur_pos]
+    def skip_comments(self): #doesn't yet support multi line
+        while self.source[self.cur_pos] != "\n":
+            self.next_char()
+        self.cur_char = self.source[self.cur_pos]
     def getToken(self): #Will do most of the work, call other methods
         pass
+    
+class token:
+    def __init__(self, val, type):
+        self.val = val
+        self.type = type
 
     
-    
+        
+class TokenType(enum.Enum):
+	EOF = -1
+	NEWLINE = 0
+	NUMBER = 1
+	IDENT = 2
+	STRING = 3
+	# Keywords
+	LABEL = 101
+	GOTO = 102
+	PRINT = 103
+	INPUT = 104
+	LET = 105
+	IF = 106
+	THEN = 107
+	ENDIF = 108
+	WHILE = 109
+	REPEAT = 110
+	ENDWHILE = 111
+	# Operators
+	EQ = 201  
+	PLUS = 202
+	MINUS = 203
+	ASTERISK = 204
+	SLASH = 205
+	EQEQ = 206
+	NOTEQ = 207
+	LT = 208
+	LTEQ = 209
+	GT = 210
+	GTEQ = 211
     
